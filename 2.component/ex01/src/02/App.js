52896ac169ae5675bcc7f5457f82a444fc990b95
sunlight9344 <sunlight9344@gmail.com>
2023-10-31 16:57:18 +0900

    map

--- 2.component/ex01/src/02/App.js
+++ 2.component/ex01/src/02/App.js
@@ -2,9 +2,14 @@ import React from 'react';
 import FoodList from './FoodList';
 
 function App() {
+    const foods = [
+        {no:1, name:'PIP', count:'10'},
+        {no:2, name:'Egg', count:'20'},
+        {no:3, name:'Mk9', count:'30'},
+    ];
     return (
         <div id={'App'}>
-            <FoodList />
+            <FoodList foods={foods}/>
         </div>  
     );
 }
--- 2.component/ex01/src/02/FoodList.js
+++ 2.component/ex01/src/02/FoodList.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import FoodListItem from './FoodListItem';
 
-function FoodList(props) {
+function FoodList({foods}) {
+    // 첫번째 방법인데 이렇게는 잘 안쓴다 왜냐하면 return 아래 코드 양이 엄청 많은데 
+    // 위에서 변수를 선언하고 하나하나 찾기 힘들기 때문이다.
+    // foods.forEach((food) => {
+    //     foodItems.push(<FoodListItem name={food.name} count={food.count}/>);
+    // });
+
     return (
         <ul>
-            <FoodListItem name={'Bread'} count={'10'} />
-            <FoodListItem name={'Egg'} count={'333'} />
-            <FoodListItem name={'Crocodile'} count={'1121'} />
+            {/* 
+                key 값을 넣어줘야함 근데 왜였더라? key 값은 반복되는 element 에 그 안에 넣어주는게 좋다
+                *** 성능 *** 차이가 난다고 함 난다요
+             */}
+            {foods.map(food => { return <FoodListItem key={food.no} name={food.name} count={food.count}/>})}
         </ul>
     );
 }
