25acd35ca557b1fcfc0e436a327b1ee7a4d845cf
sunlight9344 <sunlight9344@gmail.com>
2023-11-03 09:54:35 +0900

    kanbanboard card.scss input

--- kanbanboard/frontend/src/TaskList.js
+++ kanbanboard/frontend/src/TaskList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Task from './Task';
+import styles from './assets/scss/TaskListTask.css';
 
 function TaskList({tasks}) {
     return (
@@ -7,6 +8,11 @@ function TaskList({tasks}) {
             <ul>
                 {tasks.map((task,i) => <Task key={i} no={task.no} name={task.name} done={task.done} />)}
             </ul>
+            <input
+                className={styles.TaskList__add_task}
+                type='text'
+                placeholder={'테스크 추가하기'}
+            ></input>
         </div>
     );
 }
