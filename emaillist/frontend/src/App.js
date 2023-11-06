468b71ebfaf83ebeae283330d60061c40a5fee10
sunlight9344 <sunlight9344@gmail.com>
2023-11-06 16:54:30 +0900

    emaillist insert start

--- emaillist/frontend/src/App.js
+++ emaillist/frontend/src/App.js
@@ -1,22 +1,64 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './assets/scss/App.scss';
 import RegisterForm from './RegisterForm';
 import Searchbar from './Searchbar';
 import Emaillist from './Emaillist';
-import data from './assets/json/data';
 
 function App() {
-    
-    const[emails, setEmails] = useState(data);
+    const[emails, setEmails] = useState(null);
     const searchEmail = (keyword) => {
         setEmails(data.filter(email => (email.firstName+email.lastName).includes(keyword) || email.email.includes(keyword)));
     };
 
+    const addEmail = async (email) => {
+        console.log(email);
+        fetch('/api', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify(email)
+        });
+    }
+
+    const fetchList = async () => {
+        try{const response = await fetch('/api', {
+                method: 'get',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                body: null
+            });
+            
+            if(!response.ok) {
+                throw new Error(`${response.status} ${response.statusText}`)
+            }
+
+            const json = await response.json();
+
+            if(json.result !== 'success') {
+                throw new Error(`${json.result} ${json.message}`)
+            }
+
+            setEmails(json.data);
+
+        } catch(err) {
+            console.log(err);
+        }
+    }
+
+    useEffect(() => {fetchList()},[]);
+
     return (
         <div id={'App'}>
-            <RegisterForm />
-            <Searchbar searchEmail={searchEmail}/>
-            <Emaillist emails={emails} />
+            <RegisterForm addEmail={addEmail} />
+            <Searchbar searchEmail={searchEmail} />
+            { emails === null ?
+                null :
+                <Emaillist emails={emails} />
+            }
         </div>  
     );
 }
--- emaillist/frontend/src/RegisterForm.js
+++ emaillist/frontend/src/RegisterForm.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import styles from './assets/scss/RegisterForm.scss';
 
-function RegisterForm(props) {
+function RegisterForm({addEmail}) {
     return (
-        <form className={styles.RegisterForm}>
+        <form 
+            className={styles.RegisterForm}
+            onSubmit={(e) => {
+                e.preventDefault();
+                const email = {
+                    firstName: e.target.firstName.value,
+                    lastName: e.target.lastName.value,
+                    email: e.target.email.value
+                }
+                addEmail(email);
+            }}
+            >
             <input type='text' name='firstName' placeholder='성' className={styles.InputFirstName} />
             <input type='text' name='lastName' placeholder='이름' className={styles.InputLastName} />
             <input type='text' name='email' placeholder='이메일' className={styles.InputEmail} />
