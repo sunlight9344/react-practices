86b68403cac9cb8eb78ec799f03c3522c34db049
sunlight9344 <sunlight9344@gmail.com>
2023-11-07 14:16:58 +0900

    link

---
+++ 3.routing/src/04/component/Gallery.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Link, NaviLink} from 'react-router-dom';
+
+export default function Gallery() {
+    return (
+        <div>
+            <h1>Gallery</h1>
+        </div>
+    );
+}
